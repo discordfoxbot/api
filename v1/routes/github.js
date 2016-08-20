2575b53a74e7182ee8154cb5461880709bcc0666
Fuechschen <me@fuechschen.org>
2016-08-20 02:11:58 +0200

    getting this working

--- v1/routes/github.js
+++ v1/routes/github.js
@@ -5,16 +5,18 @@ var db = require('../../db');
 app.post('/:id', (req, res)=> {
     db.models.GithubFeed.find({where: {id: req.params.id}}).then(feed=> {
         if (feed !== undefined && feed !== null) {
-            if (last_event !== req.get('X-GitHub-Delivery')) {
+            if (feed.last_event !== req.get('X-GitHub-Delivery')) {
+                res.status(204).end();
                 feed.update({last_event: req.get('X-GitHub-Delivery')});
-                feed.getChannel().then(channel=> {
-                    db.sendEvent('githubUpdate', {
-                        payload: req.body,
-                        event: req.get('X-GitHub-Event'),
-                        channel: channel.id
-                    });
-                    res.status(204).end();
-                });
+               if(['push','watch'].includes(req.get('X-GitHub-Event'))){
+                   feed.getChannel().then(channel=> {
+                       db.sendEvent('githubUpdate', {
+                           payload: req.body,
+                           event: req.get('X-GitHub-Event'),
+                           channel: channel.cid
+                       });
+                   });
+               }
             } else res.status(409).json({error: 'duplicate event'});
         } else res.status(404).json({error: 'webhook not found'});
     });
