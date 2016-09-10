bea671cb8d16adfc021b2909051d7663f55b1d69
Fuechschen <me@fuechschen.org>
2016-09-10 23:59:47 +0200

    apparently, this does not exist yet...

--- v1/routes/stats.js
+++ v1/routes/stats.js
@@ -8,7 +8,8 @@ var db = require('../../db');
 app.get('/', (req, res, next)=> {
     Promise.all([
         db.models.Guild.count({where: {online: true}}),
-        db.models.User.count({where: {online: true}}),
+        //db.models.User.count({where: {online: true}}),
+        db.redis.hget('stats', 'users').then(c=>Promise.resolve(parseInt(c))),
         db.models.Message.count({where: {created_at: {$gt: moment().subtract(1, 'minutes').toDate()}}}),
         db.redis.hget('stats', 'channels').then(c=>Promise.resolve(parseInt(c)))
     ]).spread((g, u, m, c)=> {
