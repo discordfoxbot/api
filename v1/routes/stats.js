7a5800255123ff9c14358cd2f00de6a6eb3cdc4b
Fuechschen <me@fuechschen.org>
2016-09-10 23:53:02 +0200

    hehe

--- v1/routes/stats.js
+++ v1/routes/stats.js
@@ -9,7 +9,7 @@ app.get('/', (req, res, next)=> {
         db.models.Guild.count({where: {online: true}}),
         db.models.User.count({where: {online: true}}),
         db.models.Message.count({where: {created_at: {$gt: moment().subtract(1, 'minutes').toDate()}}}),
-        db.redis.hget('stats', 'channels').then(c=>Promise.resolve(parseInt(c)));
+        db.redis.hget('stats', 'channels').then(c=>Promise.resolve(parseInt(c)))
     ]).
     spread((g, u, m, c)=> {
         res.apijson({u, g, m, c}, {context: 'Object<Stats>'});
